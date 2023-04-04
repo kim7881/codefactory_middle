16dfdd72ab3b15418836aba362403d8b2e7b4f11
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-05 00:43:38 +0900

    Restaurant detail model

--- lib/restaurant/component/restaurant_card.dart
+++ lib/restaurant/component/restaurant_card.dart
@@ -1,4 +1,5 @@
 import 'package:codefactory/common/const/colors.dart';
+import 'package:codefactory/restaurant/model/restaurant_detail_model.dart';
 import 'package:codefactory/restaurant/model/restaurant_model.dart';
 import 'package:flutter/material.dart';
 
@@ -59,6 +60,7 @@ class RestaurantCard extends StatelessWidget {
       deliveryFee: model.deliveryFee,
       ratings: model.ratings,
       isDetail: isDetail,
+      detail: model is RestaurantDetailModel ? model.detail : null,
     );
   }
 
