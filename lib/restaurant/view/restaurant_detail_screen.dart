0bc5e229c44b9f39058ef8fb8e0352943782bca1
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-18 14:55:39 +0900

    RatingCardHeader

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:codefactory/common/const/data.dart';
 import 'package:codefactory/common/dio/dio.dart';
 import 'package:codefactory/common/layout/default_layout.dart';
 import 'package:codefactory/product/component/product_card.dart';
+import 'package:codefactory/rating/component/rating_card.dart';
 import 'package:codefactory/restaurant/component/restaurant_card.dart';
 import 'package:codefactory/restaurant/model/restaurant_detail_model.dart';
 import 'package:codefactory/restaurant/model/restaurant_model.dart';
@@ -56,6 +57,20 @@ class _RestaurantDetailScreenState
           if (state is RestaurantDetailModel) renderLabel(),
           if (state is RestaurantDetailModel)
             renderProducts(products: state.products),
+          const SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            sliver: SliverToBoxAdapter(
+              child: RatingCard(
+                rating: 4,
+                email: 'jc@codefactory.ai',
+                images: [],
+                avatarImage: AssetImage(
+                  'asset/img/logo/codefactory_logo.png',
+                ),
+                content: '맛있습니다',
+              ),
+            ),
+          ),
         ],
       ),
     );
