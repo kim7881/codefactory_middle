7d9f910c1355c29405665609767a72f0ca32b992
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-18 15:04:49 +0900

    RatingCardBody

--- lib/rating/component/rating_card.dart
+++ lib/rating/component/rating_card.dart
@@ -38,7 +38,10 @@ class RatingCard extends StatelessWidget {
           email: email,
           rating: rating,
         ),
-        _Body(),
+        const SizedBox(height: 8.0),
+        _Body(
+          content: content,
+        ),
         _Images(),
       ],
     );
@@ -90,11 +93,28 @@ class _Header extends StatelessWidget {
 }
 
 class _Body extends StatelessWidget {
-  const _Body({Key? key}) : super(key: key);
+  final String content;
+
+  const _Body({
+    Key? key,
+    required this.content,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Flexible(
+          child: Text(
+            content,
+            style: TextStyle(
+              color: BODY_TEXT_COLOR,
+              fontSize: 14.0,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
 
