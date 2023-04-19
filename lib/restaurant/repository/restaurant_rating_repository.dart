df32c0d2c991b6697dae8c8470dd5f852cb2f183
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-19 16:02:24 +0900

    RestaurnatRatingRepository

---
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -0,0 +1,23 @@
+import 'package:codefactory/common/model/cursor_pagination_model.dart';
+import 'package:codefactory/common/model/pagination_params.dart';
+import 'package:codefactory/rating/model/rating_model.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/retrofit.dart';
+
+part 'restaurant_rating_repository.g.dart';
+
+// http://ip/restaurant/:rid/rating
+@RestApi()
+abstract class RestaurantRatingRepository{
+  factory RestaurantRatingRepository(Dio dio, {String baseUrl}) =
+      _RestaurantRatingRepository;
+
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<CursorPagination<RatingModel>> paginate({
+    // 쿼리문
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
+}
