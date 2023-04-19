4a66dbbb1904b8c6f706a9769520f0a9e8bc953e
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-19 16:29:42 +0900

    RestaurantRatingProvider

---
+++ lib/restaurant/provider/restaurant_rating_provider.dart
@@ -0,0 +1,16 @@
+import 'package:codefactory/common/model/cursor_pagination_model.dart';
+import 'package:codefactory/restaurant/repository/restaurant_rating_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class RestaurantRatingStateNotifier
+    extends StateNotifier<CursorPaginationBase> {
+  final RestaurantRatingRepository repository;
+
+  RestaurantRatingStateNotifier({
+    required this.repository,
+  }) : super(
+          CursorPaginationLoading(),
+        );
+
+
+}
