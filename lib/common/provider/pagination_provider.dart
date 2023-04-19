7b7e9d94c3c3883ddc7247d9e9df1cf7f220e15c
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-19 17:20:51 +0900

    Pagination model 일반화

--- lib/common/provider/pagination_provider.dart
+++ lib/common/provider/pagination_provider.dart
@@ -1,9 +1,13 @@
 import 'package:codefactory/common/model/cursor_pagination_model.dart';
+import 'package:codefactory/common/model/model_with_id.dart';
 import 'package:codefactory/common/model/pagination_params.dart';
 import 'package:codefactory/common/repository/base_pagination_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PaginationProvider<U extends IBasePaginationRepository> extends StateNotifier<CursorPaginationBase> {
+class PaginationProvider<
+T extends IModelWithId,
+U extends IBasePaginationRepository<T>
+> extends StateNotifier<CursorPaginationBase> {
   final U repository;
 
   PaginationProvider({
@@ -64,7 +68,7 @@ class PaginationProvider<U extends IBasePaginationRepository> extends StateNotif
       // fetchMore
       // 데이터를 추가로 더 가져오는 상황
       if (fetchMore) {
-        final pState = state as CursorPagination;
+        final pState = state as CursorPagination<T>;
 
         state = CursorPaginationFetchingMore(
           meta: pState.meta,
@@ -80,9 +84,9 @@ class PaginationProvider<U extends IBasePaginationRepository> extends StateNotif
         // 만약에 데이터가 있는 상황이라면
         // 기존 데이터를 보존한채로 Fetch(API 요청)를 진행
         if (state is CursorPagination && !forceRefetch) {
-          final pState = state as CursorPagination;
+          final pState = state as CursorPagination<T>;
 
-          state = CursorPaginationRefetching(
+          state = CursorPaginationRefetching<T>(
             meta: pState.meta,
             data: pState.data,
           );
@@ -99,7 +103,7 @@ class PaginationProvider<U extends IBasePaginationRepository> extends StateNotif
       );
 
       if (state is CursorPaginationFetchingMore) {
-        final pState = state as CursorPaginationFetchingMore;
+        final pState = state as CursorPaginationFetchingMore<T>;
 
         // 기존 데이터에
         // 새로운 데이터 추가
