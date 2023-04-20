264327f03ad1723a5b3185fe8edc8bfa423a1d3d
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-21 01:15:52 +0900

    존재하지 않는 데이터 캐시에 입력하기

--- lib/product/view/product_screen.dart
+++ lib/product/view/product_screen.dart
@@ -2,6 +2,7 @@ import 'package:codefactory/common/component/pagination_list_view.dart';
 import 'package:codefactory/product/component/product_card.dart';
 import 'package:codefactory/product/model/product_model.dart';
 import 'package:codefactory/product/provider/product_provider.dart';
+import 'package:codefactory/restaurant/view/restaurant_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -20,8 +21,19 @@ class ProductScreen extends StatelessWidget {
     return PaginationListView<ProductModel>(
       provider: productProvider,
       itemBuilder: <ProductModel>(_, index, model) {
-        return ProductCard.fromProductModel(
-          model: model,
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => RestaurantDetailScreen(
+                  id: model.restaurant.id,
+                ),
+              ),
+            );
+          },
+          child: ProductCard.fromProductModel(
+            model: model,
+          ),
         );
       },
     );
