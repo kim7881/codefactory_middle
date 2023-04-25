fcfe3a1f4a982df41a739290b8041b2c917d6d3b
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-26 01:01:19 +0900

    Login, Token Refresh Repository

---
+++ lib/user/repository/auth_repository.dart
@@ -0,0 +1,46 @@
+import 'package:codefactory/common/model/login_response.dart';
+import 'package:codefactory/common/model/token_response.dart';
+import 'package:codefactory/common/utils/data_utils.dart';
+import 'package:dio/dio.dart';
+
+class AuthRepository {
+  // http://$ip/auth
+  final String baseUrl;
+  final Dio dio;
+
+  AuthRepository({
+    required this.baseUrl,
+    required this.dio,
+  });
+
+  Future<LoginResponse> login({
+    required String username,
+    required String password,
+  }) async{
+    final serialized = DataUtils.plainToBase64('$username:$password');
+
+    final resp = await dio.post(
+      '$baseUrl/login',
+      options: Options(
+        headers: {
+          'authorization': 'Basic $serialized'
+        }
+      ),
+    );
+
+    return LoginResponse.fromJson(resp.data);
+  }
+
+  Future<TokenResponse> token() async{
+    final resp = await dio.post(
+      '$baseUrl/token',
+      options: Options(
+        headers: {
+          'refreshToken': 'true'
+        },
+      ),
+    );
+
+    return TokenResponse.fromJson(resp.data);
+  }
+}
